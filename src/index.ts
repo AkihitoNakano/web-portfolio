567d380e5b96984c64daca03c62cb296b623ac48
AkihitoNakano <linkin_087@yahoo.co.jp>
2022-08-17 23:39:31 +0900

    取り敢えず要素を全部入れてみたけど面白さがないからやり直しが必要

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
-import './style.css'
-import './images/me.jpeg'
-import './images/github.png'
-import './images/twitter.png'
+import './ts/importImages.ts'
+
+const popUpTemplateEl = document.getElementById('modal')! as HTMLTemplateElement
+const popupImgs = document.querySelectorAll('.popup-img')!
+
+popupImgs.forEach(img => {
+  img.addEventListener('click', e => {
+    e.preventDefault
+    popupImage()
+  })
+})
+
+function popupImage(): void {}
+
+// class PopupImg {
+//   templateElement: HTMLTemplateElement
+//   hostElement: HTMLTemplateElement
+//   imageElements: HTMLDivElement
+
+//   constructor(templateId: string, hostElementId: string) {
+//     this.templateElement = document.getElementById('modal')! as HTMLTemplateElement
+//     this.hostElement = document.querySelector('main')! as HTMLTemplateElement
+//     this.imageElements = document.querySelectorAll('.popup-img')!
+
+//     this.imageElements.addEventListener('click', () => {
+//       console.log('hi')
+//     })
+
+//     const importNode = document.importNode(this.templateElement.content, true)
+//     this.attachListener()
+//   }
+
+//   attachListener() {
+//     this.imageElements.addEventListener('click', e => {
+//       e.preventDefault
+//     })
+//   }
+// }
